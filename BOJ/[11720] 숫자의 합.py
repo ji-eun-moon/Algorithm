b08309d66d4c8f0c965ed4c272b45586d6371a4b
문지은 <mjieun0956@gmail.com>
2023-03-19 01:14:45 +0900

    BOJ PS

---
+++ BOJ/[11720] 숫자의 합.py
@@ -0,0 +1,7 @@
+N = int(input())
+lst = list(input())
+Sum = 0
+for i in range(N):
+    Sum += int(lst[i])
+
+print(Sum)
---
+++ BOJ/[1546] 평균.py
@@ -0,0 +1,7 @@
+N = int(input())
+scores = list(map(int, input().split()))
+M = max(scores)
+for i in range(N):
+    scores[i] = scores[i]/M*100
+ans = sum(scores)/N
+print(ans)
