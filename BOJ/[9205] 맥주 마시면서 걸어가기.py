6cc1d44e0c15416e00a02a5fac61c04f47ade66f
문지은 <mjieun0956@gmail.com>
2023-03-23 14:37:01 +0900

    BOJ PS

---
+++ BOJ/[9205] 맥주 마시면서 걸어가기.py
@@ -0,0 +1,10 @@
+T = int(input())
+for test_case in range(1, T+1):
+    N = int(input())  # 편의점 개수
+    sty, stx = map(int, input().split())  # 집
+    by, bx = map(int, input().split())  # 편의점
+    edy, edx = map(int, input().split())  # 페스티벌
+
+    beer = 20
+
+
