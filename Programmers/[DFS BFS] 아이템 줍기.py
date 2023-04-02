063cc95ac4b07e77f8e9524fc382ad6ecde6009d
문지은 <mjieun0956@gmail.com>
2023-04-02 17:50:06 +0900

    programmers PS

--- Programmers/[DFS BFS] 아이템 줍기.py
+++ Programmers/[DFS BFS] 아이템 줍기.py
@@ -1,11 +1,51 @@
+# 직사각형
 rectangle = [[1,1,7,4],[3,2,5,5],[4,3,6,9],[2,6,8,8]]
+# 초기 캐릭터 위치
 characterX = 1
 characterY = 3
+# 아이템 위치
 itemX = 7
 itemY = 8
 
 def solution(rectangle, characterX, characterY, itemX, itemY):
-    answer = 0
+    N = 104
+    arr = [[0] * N for _ in range(N)]
+
+    # 사각형 칠하기
+    for rec in rectangle:
+        x1, y1, x2, y2 = rec
+        for i in range(y1 * 2, y2 * 2 + 1):
+            for j in range(x1 * 2, x2 * 2 + 1):
+                arr[i][j] = 1
+
+    # 사각형 내부 0으로 바꾸기
+    for rec in rectangle:
+        x1, y1, x2, y2 = rec
+        for i in range(y1 * 2 + 1, y2 * 2):
+            for j in range(x1 * 2 + 1, x2 * 2):
+                arr[i][j] = 0
+
+    used = [[0] * N for _ in range(N)]
+    Min = 21e8
+
+    def bfs():
+        direct_y = [-1, 1, 0, 0]
+        direct_x = [0, 0, -1, 1]
+        q = []
+        q.append((characterY * 2, characterX * 2))
+        used[characterY * 2][characterX * 2] = 1
+        while q:
+            y, x = q.pop(0)
+            if y == itemY * 2 and x == itemX * 2:
+                return used[y][x] // 2
+            for i in range(4):
+                dy = y + direct_y[i]
+                dx = x + direct_x[i]
+                if 0 <= dy < N and 0 <= dx < N:
+                    if used[dy][dx] == 0 and arr[dy][dx] == 1:
+                        used[dy][dx] = used[y][x] + 1
+                        q.append((dy, dx))
+    answer = bfs()
     return answer
 
 print(solution(rectangle, characterX, characterY, itemX, itemY))
--- Programmers/[DFS BFS] 여행경로.py
+++ Programmers/[DFS BFS] 여행경로.py
@@ -1,48 +1,47 @@
-tickets = [["ICN", "JFK"], ["ICN", "AAD"], ["JFK", "ICN"]]
+tickets = [["ICN", "AAA"], ["BBB", "ICN"], ["ICN", "BBB"]]
 def solution(tickets):
+    global path, answer
     N = len(tickets)
-    dic = {}
     answer = []
 
-    for ticket in tickets:
-        dic.setdefault(ticket[0], [])
-        dic[ticket[0]] = ticket[1]
+    # ICN ticket 찾기 - 시작
+    icn = []
+    for i in range(N):
+        if tickets[i][0] == 'ICN':
+            idx = i
+            icn.append(idx)
 
-    def bfs():
+    def dfs(idx):
+        global path, answer
+        # 출발지, 도착지
+        st = tickets[idx][0]
+        ed = tickets[idx][1]
+        # path 배열의 길이가 N이면 티켓을 끝까지 사용하는 것을 성공했으므로 answer 배열에 담기
+        # 이 때 마지막 ed가 안담겨있으므로 추가해주어야 함
+        if len(path) == N:
+            answer.append(path[:]+[ed])
+            return
+        for i in range(N):
+            # 아직 사용하지 않은 티켓이고 티켓의 도착지와 다음 티켓의 출발지가 같다면
+            if used[i] == 0 and tickets[i][0] == ed:
+                path.append(ed)
+                used[i] = 1
+                dfs(i)
+                path.pop()
+                used[i] = 0
+
+    # 가능한 시작 지점 전부 탐색, 탐색할때마다 used, path 배열 초기화
+    for i in range(len(icn)):
         used = [0] * N
-        temp = []
-        q = []
-        q.append(['ICN'])
-        while q:
-            st = q.pop(0)
+        idx = icn[i]
+        used[idx] = 1
+        path = ['ICN']
+        dfs(idx)
 
-            # 갈 수 있는 도착지 없으면 continue
-            if dic.get(st) == None:
-                continue
-            print(temp)
-            ed = dic[st]
-            # 표 인덱스 찾기
-            for i in range(N):
-                if tickets[i] == [st, ed]:
-                    idx = i
-
-            # 티켓 전부다 확인했는지 체크하고 answer에 넣기
-            if len(temp) == N:
-                answer.append(temp)
-
-            if used[idx] == 0:
-                used[idx] = 1
-                q.append(ed)
-
-            temp.append(dic[st])
-
-
-        return temp
-
-
+    # 알파벳 순으로 정렬
     answer.sort()
-
-    return bfs()
+    # 가장 앞에꺼가 정답
+    return answer[0]
 
 print(solution(tickets))
 
