ed32fa4a3c6013ddbe2246446279a712160d4df3
문지은 <mjieun0956@gmail.com>
2023-07-23 06:58:58 +0900

    [BOJ] 1062 - 가르침

--- BOJ/[1062] 가르침.py
+++ BOJ/[1062] 가르침.py
@@ -1,8 +1,10 @@
+from itertools import combinations
 import sys
 input = sys.stdin.readline
 
 N, K = map(int, input().split())  # 단어 개수, 사용할 수 있는 알파벳 수
 words = set(input().rstrip()[4:-4] for _ in range(N))
+default = {'a','n','t','i','c'}
 
 # anta로 시작하고 tica로 끝나므로 최소로 필요한 단어 수는 5개 (a, c, i, n, t)
 # K 가 5보다 작으면 어떤 언어도 배울 수 없음
@@ -14,33 +16,32 @@ elif K == 26:
     print(N)
     exit()
 
+DAT = [0]*123
+for c in ('a', 'c', 'i', 'n', 't'):
+    DAT[ord(c)] = 1
+
 # a, c, i, n, t를 제외한 26개 중 K-5 개의 알파벳 뽑기 (조합)
 # 해당 조합으로 단어를 만들 수 있는지 확인하여 개수 세기
-def dfs(idx, level):
-    global Max
-    if level == K-5:  # 알파벳 다 뽑았으면
-        # 만들 수 있는 단어 최대 개수 확인하기
-        cnt = 0
-        for word in words:
-            for w in set(word).difference('a', 'c', 'i', 'n', 't'):
-                if DAT[ord(w) - ord('a')] == 0:
-                    break
-            else:
-                cnt += 1
-        Max = max(Max, cnt)
-        return
+def cntWords():
+    cnt = 0
+    for word in words:
+        for w in word:
+            if DAT[ord(w)] == 0:
+                break
+        else:
+            cnt += 1
+    return cnt
 
-    # 알파벳 뽑기 - 조합
-    for i in range(idx, 26):
-        if DAT[i] == 0:
-            DAT[i] = 1
-            dfs(idx+1, level+1)
-            DAT[i] = 0
-
-DAT = [0]*26
-for c in ('a', 'c', 'i', 'n', 't'):
-    DAT[ord(c)-ord('a')] = 1
-
+remain_alphabet = set(chr(i) for i in range(97, 123)) - default
 Max = 0
-dfs(0, 0)
+
+for teach_word in list(combinations(remain_alphabet, K-5)):
+    for t in teach_word:
+        DAT[ord(t)] = 1
+
+    Max = max(Max, cntWords())
+
+    for t in teach_word:
+        DAT[ord(t)] = 0
+
 print(Max)
