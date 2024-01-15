8decd5a74fb2fab8bc4ed135da02f5b4d5782bc1
문지은 <mjieun0956@gmail.com>
2024-01-16 00:28:35 +0900

    [Programmers] 양과 늑대

--- Programmers/양과 늑대.py
+++ Programmers/양과 늑대.py
@@ -1,33 +1,22 @@
 def solution(info, edges):
-    N = len(info)
-    answer = 0
+    visited = [0] * len(info)
+    answer = []
 
-    tree = {i : [] for i in range(N)}
-    for p, c in edges:
-        tree[p].append(c)
-
-    visited = [0]*N
-    def dfs(start, sheep, wolf):
-        nonlocal answer
-
-        if wolf >= sheep:
+    def dfs(sheep, wolf):
+        if sheep > wolf:
+            answer.append(sheep)
+        else:
             return
 
-        answer = max(answer, sheep)
-
-        for nxt in tree[start]:
-            if not visited[nxt]:
-                visited[nxt] = 1
-                if info[nxt] == 0:
-                    dfs(nxt, sheep+1, wolf)
+        for p, c in edges:
+            if visited[p] and not visited[c]:
+                visited[c] = True
+                if info[c] == 0:
+                    dfs(sheep + 1, wolf)
                 else:
-                    dfs(nxt, sheep, wolf+1)
-                visited[nxt] = 0
+                    dfs(sheep, wolf + 1)
+                visited[c] = False
+    visited[0] = True
+    dfs(1, 0)
 
-    visited[0] = 1
-    dfs(0, 1, 0)
-
-    return answer
-
-print('#1', solution([0,0,1,1,1,0,1,0,1,0,1,1], [[0,1],[1,2],[1,4],[0,8],[8,7],[9,10],[9,11],[4,3],[6,5],[4,6],[8,9]]))
-print('#2', solution([0,1,0,1,1,0,1,0,0,1,0], [[0,1],[0,2],[1,3],[1,4],[2,5],[2,6],[3,7],[4,8],[6,9],[9,10]]))
+    return max(answer)
