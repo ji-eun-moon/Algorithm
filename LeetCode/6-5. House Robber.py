c63898e368980952d22a0fd43d59b0cd8e1707ca
문지은 <mjieun0956@gmail.com>
2024-03-02 23:40:05 +0900

    [LeetCode] House Robber

--- LeetCode/6-5. House Robber.py
+++ LeetCode/6-5. House Robber.py
@@ -1,7 +1,39 @@
+
+# Top down
 class Solution:
     def rob(self, nums):
-        answer = 0
-        return answer
+        n = len(nums)
+
+        memo = {}
+        def dp(i):
+            if i == 0:
+                return nums[0]
+            if i == 1:
+                return max(nums[0], nums[1])
+
+            if i not in memo:
+                memo[i] = max(dp(i-1), dp(i-2)+nums[i])
+
+            return memo[i]
+
+        return dp(n-1)
+
+# Bottom up
+class Solution:
+    def rob(self, nums):
+        n = len(nums)
+
+        dp = [-1]*n
+        dp[0] = nums[0]
+
+        if n > 1:
+            dp[1] = max(nums[0], nums[1])
+
+            for i in range(2, n):
+                dp[i] = max(dp[i-1], dp[i-2]+nums[i])
+
+        return dp[n-1]
+
 
 solve = Solution()
 print(solve.rob(nums = [1,2,3,1]))
