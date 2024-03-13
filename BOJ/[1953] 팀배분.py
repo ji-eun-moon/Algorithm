5ef525dcce42c091d4b9520b0d3a9c4adaa33a83
문지은 <mjieun0956@gmail.com>
2024-03-14 00:35:00 +0900

    [BOJ] 1953 - 팀 배분

--- BOJ/[1953] 팀배분.py
+++ BOJ/[1953] 팀배분.py
@@ -1,19 +1,72 @@
-from collections import deque
+# DFS
+def dfs(cur, team):
+    visited[cur] = 1
+    result[cur] = team
+    for nxt in range(1, N+1):
+        if nxt in hate[cur] and not visited[nxt]:
+            dfs(nxt, -team)
 
 N = int(input())  # 학생의 수
 
 hate = {}  # 싫어하는 사람 정보
 
-for i in range(N):
-    hate[i+1] = set(map(int, input().split()))  # i+1 가 싫어하는 사람들
+for i in range(1, N+1):
+    hate[i] = set(list(map(int, input().split()))[1:])  # i가 싫어하는 사람 수, 사람들
+
+# A가 B를 싫어하면 B도 A를 싫어한다.
+for i in range(1, N+1):
+    for j in hate[i]:
+        hate[j].add(i)
 
 blue, white = [], []
-visited = [0]*(N+1)
-result = ["blue"] * (N+1)
+visited = [0] * (N+1)
+result = [0] * (N+1)
+
+for i in range(1, N+1):
+    if visited[i] == 0:
+        dfs(i, 1)
+
+for i in range(1, N+1):
+    if result[i] == -1:
+        blue.append(i)
+    elif result[i] == 1:
+        white.append(i)
+
+# 각 팀 오름차순 정렬
+blue.sort()
+white.sort()
+
+print(len(blue))
+print(*blue)
+print(len(white))
+print(*white)
+
+
+# BFS
+from collections import deque
+
+N = int(input())  # 학생의 수
+
+hate = {} # 싫어하는 사람 정보
+
+for i in range(1, N+1):
+    hate[i] = set(list(map(int, input().split()))[1:])  # i+1 가 싫어하는 사람 수, 사람들
+
+# A가 B를 싫어하면 B도 A를 싫어한다.
+for i in range(1, N+1):
+    for j in hate[i]:
+        hate[j].add(i)
+
+blue, white = [], []
+visited = [0] * (N+1)
+result = [0] * (N+1)
 
 def bfs(cur, team):
+
     q = deque()
     q.append((cur, team))
+    visited[cur] = 1
+    result[cur] = team
 
     while q:
         cur, team = q.popleft()
@@ -21,17 +74,17 @@ def bfs(cur, team):
             # 싫어하는 사람이면 다른 팀에 배정
             if nxt in hate[cur] and not visited[nxt]:
                 visited[nxt] = 1
-                result[nxt] = "white"
-                q.append((nxt, "blue"))
+                result[nxt] = -team
+                q.append((nxt, -team))
 
 for i in range(1, N+1):
-    if not visited[i]:
-        bfs(i, "blue")
+    if visited[i] == 0:
+        bfs(i, 1)
 
 for i in range(1, N+1):
-    if result[i] == "blue":
+    if result[i] == -1:
         blue.append(i)
-    elif result[i] == "white":
+    elif result[i] == 1:
         white.append(i)
 
 # 각 팀 오름차순 정렬
